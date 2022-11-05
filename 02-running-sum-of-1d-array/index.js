6ccfde089e60f78a501b1785aa96272551fb6df6
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-11-05 12:20:07 -0500

    02 running sum of 1d array was kind of tricky

---
+++ 02-running-sum-of-1d-array/index.js
@@ -0,0 +1,15 @@
+var runningSum = function(nums) {
+    var runningSums = []
+    var accumulator = 0
+    for(var i = 0; nums.length > i; i++){
+        for(j = i; j >= 0; j--) {
+            accumulator += nums[j]
+        }
+        runningSums.push(accumulator)
+        accumulator = 0
+    }
+    console.log(runningSums)
+    return runningSums;    
+};
+
+runningSum([1,1,1,1,1]);
