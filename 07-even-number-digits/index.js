86159223745666bff17d22f1472fbd45d10f3977
Andrew Gelstein <Andrew.Gelstein@nisc.coop>
2022-11-21 13:25:07 -0600

    07 counting the digits in a given array can be tricky but this is a fairly clean way

---
+++ 07-even-number-digits/index.js
@@ -0,0 +1,17 @@
+function findNumbers(nums) {
+    var count = 0;
+    var digitCount = 0;
+    for(var i = 0; nums.length > i; i++){
+        if (nums[i] >= 1) ++digitCount;
+        while (nums[i]/ 10 >= 1) {
+            nums[i] /= 10;
+            ++digitCount;
+        }
+        if(digitCount % 2 == 0) ++count;
+        digitCount = 0
+    }
+    console.log(count)
+    return count
+}
+
+findNumbers([12,345,2,6,7896])
