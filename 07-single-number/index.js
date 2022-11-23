06f4e62244b83783723bca1bdb9446a0784b9702
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-11-22 18:41:54 -0600

    07 GOT IT. Good push.

--- 07-single-number/index.js
+++ 07-single-number/index.js
@@ -1,34 +1,16 @@
 // 136. Single Number
 var singleNumber = function(nums) {
-    let uniques = []
+    const uniques = {}
     let number
     for (var i = 0 ; i < nums.length; i++) {
-        let number = nums[i]
-        console.log(number)
-        // gotta write a check to see if it's in uniques
-
-        uniques.push(number)
-
-        // console.log(number, 'number to compare')
-        // console.log(matchingNumber, 'index')
-
-        // var matchingNumber = nums.indexOf(number)
-
-        // push only unique numbers to the array
-        // if you find they are not unique remove them from the array
-
-        // for (var j = i ; j < nums.length; j++) {
-        //     console.log('inner compare')
-        //     let v = number
-        // }
-        
-        // pick the number and store it
-        // iterate through and keep doing this until we find a unique number
-
-
+        let numberKey = nums[i]
+        if(!(numberKey in uniques)) {
+            uniques[numberKey] = 0;
+        } else {
+            delete uniques[numberKey] 
+        }
     }
-
-    return number
-};
+    return [Object.keys(uniques)[0]]
+}
 
 singleNumber([2, 2, 1])
