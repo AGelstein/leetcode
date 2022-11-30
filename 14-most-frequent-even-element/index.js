55f8b3730eca1d30ed9a42a927688e4abc5cf2a0
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-11-30 17:54:18 -0600

    14 working through a nasty one.

--- 14-most-frequent-even-element/index.js
+++ 14-most-frequent-even-element/index.js
@@ -2,6 +2,37 @@
 
 var mostFrequentEven = function(nums) {
     // make new array with only evens or return -1
+    let evenArray = []
+    let currentEvenIndex = 0
+    let frequency = 0
+    let mostFrequentEven = 0
+    // this is just for the getting the evens
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] % 2 === 0) {
+            evenArray.push(nums[i])
+        }
+    }
+    // sort the array
+    const sortedArray = evenArray.sort(((a,b) => a-b))
+
+    // this is for adding up the repeats
+    for(let j = 0; j < sortedArray.length; j++) {
+        frequency = 0
+        currentEvenIndex = j
+        for(let i = currentEvenIndex; i < sortedArray.length; i++) {
+            if(sortedArray[currentEvenIndex] == sortedArray[i]){
+                frequency += 1
+            }
+            console.log(sortedArray[i])
+            console.log(frequency, 'frequency')
+        
+        }
+
+    }
+
+    // most frequent even element
+    // if there's a tie return the smallest one
+    // if there is no such element return -1
 };
 
-mostFrequentEven([0,1,2,2,4,4,1])
+mostFrequentEven([1,2,2,4,4,1,0])
