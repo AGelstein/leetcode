d33a440c7d41839d158cf6dc3b2b903a1c8e8fee
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-11-30 18:05:10 -0600

    push changes again

--- 14-most-frequent-even-element/index.js
+++ 14-most-frequent-even-element/index.js
@@ -3,35 +3,30 @@
 var mostFrequentEven = function(nums) {
     // make new array with only evens or return -1
     let evenArray = []
-    let currentEvenIndex = 0
+    let currentEvenNumber = 0
     let currentBiggestAmount = 0
     let frequency = 0
-    let mostFrequentEven = 0
-    // this is just for the getting the evens
     for(let i = 0; i < nums.length; i++) {
         if(nums[i] % 2 === 0) {
             evenArray.push(nums[i])
         }
     }
-    // sort the array
     const sortedArray = evenArray.sort(((a,b) => a-b))
 
-    // this is for adding up the repeats
     for(let j = 0; j < sortedArray.length; j++) {
         frequency = 0
         currentEvenIndex = j
         for(let i = currentEvenIndex; i < sortedArray.length; i++) {
-            if(sortedArray[currentEvenIndex] == sortedArray[i]){
+            if(sortedArray[currentEvenIndex] == sortedArray[i]) {
                 frequency += 1
             }
         }
             if(frequency > currentBiggestAmount)
             {
                 currentBiggestAmount = frequency
-                currentEvenIndex = i
+                // need to set a global var for the actual number associated with the frequency
             }
             console.log(currentBiggestAmount, 'currentBiggestAmount')
-
         }
 
     // most frequent even element
